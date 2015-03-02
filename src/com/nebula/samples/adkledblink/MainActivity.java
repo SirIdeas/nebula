c6610836c1cc448deeef219ee6516d5ce3af6b2d
unknown <arondn2@gmail.com>
2015-03-01 19:43:03 -0430

    actualizar android-support-v4

--- src/com/nebula/samples/adkledblink/MainActivity.java
+++ src/com/nebula/samples/adkledblink/MainActivity.java
@@ -12,47 +12,48 @@ import android.widget.ToggleButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
 public class MainActivity extends NbAdkMainActivityHelper {
-	  
-	// Instanciar un LED Digital
-	private NbLedDigital led = new NbLedDigital(13);
+    
+  // Instanciar un LED Digital
+  private NbLedDigital led = new NbLedDigital(13);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	    
-	    // Conectar el led al Sketch
-	    getSketch().connect(led);
-	    
-	    // Asignar listener al ToggleButton para encender y apagar el LED
-	    ((ToggleButton)findViewById(R.id.toggleButton1)).setOnCheckedChangeListener(new OnCheckedChangeListener() {
-	      
-	      @Override
-	      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-	        led.setValue(isChecked);
-	      }
-	      
-	    });
-	    
-	    NbButton b = new NbButton(14);
-	    NbTrimmer t = new NbTrimmer(0);
-	    
-	    b.setOnStateChangeListener(new NbCmp.OnStateChangeListener() {
-			
-			@Override
-			public void onStateChange(NbCmp component, boolean newState) {
-//				led.toggle();
-			}
-		});
-	    
-	    t.setOnValueChangeListener(new NbCmp.OnValueChangeListener() {
-			
-			@Override
-			public void onValueChange(NbCmp component, int newValue, int oldValue) {
-				led.setValue((int)((newValue * 255) / 1023));
-			}
-		});
-	    
-	}
-	
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+      
+    // Conectar el led al Sketch
+    getSketch().connect(led);
+      
+    // Asignar listener al ToggleButton para encender y apagar el LED
+    ((ToggleButton)findViewById(R.id.toggleButton1)).setOnCheckedChangeListener(new OnCheckedChangeListener() {
+        
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        led.setValue(isChecked);
+      }
+        
+    });
+      
+    NbButton b = new NbButton(14);
+    NbTrimmer t = new NbTrimmer(0);
+      
+    b.setOnStateChangeListener(new NbCmp.OnStateChangeListener() {
+      
+      @Override
+      public void onStateChange(NbCmp component, boolean newState) {
+//        led.toggle();
+      }
+    });
+      
+    t.setOnValueChangeListener(new NbCmp.OnValueChangeListener() {
+      
+      @Override
+      public void onValueChange(NbCmp component, int newValue, int oldValue) {
+        led.setValue((int)((newValue * 255) / 1023));
+      }
+      
+    });
+      
+  }
+  
 }
