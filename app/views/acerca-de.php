1a3a62ed3c7d548e0efdb6684a500591b94cabe0
unknown <arondn2@gmail.com>
2015-03-02 17:53:40 -0430

    vista de acerca de

--- app/views/acerca-de.php
+++ app/views/acerca-de.php
@@ -4,5 +4,50 @@
 
 <h1>Acerca de</h1>
 <p>
-  
+  Proyecto desarrollado por Alexander Rondón (<a href="https://twitter.com/arondn2">arondn2</a>) y Guillermo Seminario (<a href="https://twitter.com/gjseminario">gjseminario</a>), asesorado por la Dra. Luz Marina Prada para optar al título de Ingeniería en Informática en la <a href="http://www.uneg.edu.ve/htmls/?t&p=portal.html">Universidad Nacional Experimental de Guayana (UNEG)</a>, bajo el nombre: Plataforma de <strong>PLATAFORMA PARA PRÁCTICAS DE SISTEMAS DE CONTROL CON DISPOSITIVOS MÓVILES INTELIGENTES ANDROID PARA EL LABORATORIO DE AUTOMATIZACIÓN INDUSTRIAL DE LA UNIVERSIDAD NACIONAL EXPERIMENTAL DE GUAYANA (UNEG)</strong>.
+</p>
+
+<h3>Resumen</h3>
+<p>
+  El Laboratorio de Automatización Industrial de la Universidad Nacional Experimental de Guayana, presenta dificultades al momento de desarrollar proyectos de automatización que involucren dispositivos móviles inteligentes; ya que no dispone de herramientas que permitan poner en práctica el desarrollo de proyectos sobre nuevas tecnologías como la telefonía móvil. La investigación tuvo como objetivo general proponer una plataforma de prácticas para proyectos de sistemas de control que involucren el uso de dispositivos móviles inteligentes con el sistema operativo Android. Para ello, se construyeron componentes de hardware aunado al desarrollo de la biblioteca de software del mismo. Entre las bases teóricas  tomadas como fundamentos se encuentra el trabajo realizado por Mazurov (2011-2012) sobre el uso de Arduino como USB anfitrión. La metodología utilizada fue la investigación tecnológica con un diseño de campo, recolectando información de las plataformas existentes en el Laboratorio de Automatización Industrial (UNEG); y a su vez se efectuó una revisión documental de manuales que usan y soportan dichas plataformas. Se aplicaron técnicas e instrumentos para la recolección de datos: encuestas, observación y análisis documental. Con el análisis obtenido, se derivaron las principales características y funcionalidades que debe poseer la propuesta. La investigación arrojó como resultado una plataforma de prácticas para proyectos de automatización y control con dispositivos móviles inteligentes basados en el sistema operativo Android, la cual está a disposición del Laboratorio de Automatización Industrial de la Universidad Nacional Experimental de Guayana, la cual cuenta con una nueva herramienta que facilita la creación de proyectos de automatización y control con dispositivos móviles inteligentes.
+</p>
+<p>
+  <strong>Descriptores</strong>: Automatización, Android, Arduino, Dispositivos Móviles Inteligentes, Robótica
+</p>
+
+<h3>Dedicatoria</h3>
+
+<p>
+  <blockquote>
+    <i>A mis padres por su apoyo y ayuda incondicional<br>
+    A mis cuatro hermanos por ser un modelo a seguir para superarme<br>
+    A mi universidad y mi país porque que sé que saldremos adelante<br></i>
+    Rondón Alexander
+  </blockquote>
+</p>
+<p>
+  <blockquote>
+    <i>
+    A mi papá por ser fuente de inspiración  y modelo a seguir<br>
+    A mi mamá por apoyarme en todo<br>
+    A mis hermanos especialmente a María Teresa por ayudarme tanto<br>
+    Y a mi esposa por estar conmigo y darme ese ánimo necesario para culminar<br>
+    </i>
+    Seminario Guillermo
+  </blockquote>
+</p>
+
+<h3>Agradecimientos</h3>
+
+<p>
+  A nuestras familias en especial a nuestros padres por su total apoyo y compresión durante el largo camino que representó la elaboración de esta investigación.
+</p>
+<p>
+  A la Dra. Luz Marina Prada por ser nuestra mentora, servirnos de apoyo en los momentos en que pensamos declinar, por estar en las altas y bajas de este recorrido, y sobre todo por su dedicación y compromiso con todos los estudiantes que desean realizar investigación en nuestra casa de estudio.
+</p>
+<p>
+  <blockquote><i>A los profesores que admiro mucho: Jessy Belmonte, Yolis Domínguez, Zeudy Jiménez, Luz Waldropp, Carlos Abbafy, Dennis Moreno, Nelson Inojosa, Víctor Avendaño, Joyce Aragón y muchos otros que lamento no recordar, por su aporte en mi formación académica.</i> Rondón Alexander.</blockquote>
+</p>
+<p>
+  <blockquote><i>A los profesores: Nancy Ávila, Carlos Abbafy, Andrés Caniumilla, Alejandro Marcus, Sandra Castillo por haberme guiado y servir de modelo como profesionales y como persona.</i> Seminario Guillermo</blockquote>
 </p>
