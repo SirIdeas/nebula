f6d58c832c8bdb40de466f4eda8e9a81a5cf9516
Alex J. Rondon <alex@sirideas.com>
2016-03-12 02:48:20 -0430

    change versions

--- app/views/tpl.php
+++ app/views/tpl.php
@@ -14,10 +14,12 @@
     </title>
     <link rel="shortcut icon" href="(:/:)/favicon.ico"/>
     <link rel="stylesheet" href="(:/:)/font/flaticon.css"/>
-    <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap.css"/>
-    <!-- Bootstrap theme -->
-    <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap-theme.min.css"/>
+    <!-- Latest compiled and minified CSS -->
+    <!-- <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap.css"/> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <!-- Optional theme -->
+    <!-- <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap-theme.min.css"/> -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link rel="stylesheet" href="(:/:)/vendor/ie10-viewport-bug-workaround.css">
     <!-- Prism styles -->
@@ -46,8 +48,11 @@
       })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
       ga('create', 'UA-41615550-12', 'auto');ga('send', 'pageview');
     </script>
-    <script type="text/javascript" src="(:/:)/bower_components/jquery/dist/jquery.js"></script>
-    <script type="text/javascript" src="(:/:)/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- <script type="text/javascript" src="(:/:)/bower_components/jquery/dist/jquery.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- <script type="text/javascript" src="(:/:)/bower_components/bootstrap/dist/js/bootstrap.js"></script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     <!-- <script type="text/javascript" src="(:/:)/vendor/docs.min.js"></script> -->
     <script type="text/javascript" src="(:/:)/vendor/ie10-viewport-bug-workaround.js"></script>
     <script type="text/javascript" src="(:/:)/vendor/prism/prism.js"></script>
