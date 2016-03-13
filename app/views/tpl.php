e0316552f948a7c050ca810c06fadafba9043d0b
Alex J. Rondon <alex@sirideas.com>
2016-03-13 14:50:54 -0430

    revision tpl docs

--- app/views/tpl.php
+++ app/views/tpl.php
@@ -18,18 +18,17 @@
     <!-- Latest compiled and minified CSS -->
     <!-- <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap.css"/> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <!-- Optional theme -->
     <!-- <link rel="stylesheet" href="(:/:)/bower_components/bootstrap/dist/css/bootstrap-theme.min.css"/> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link rel="stylesheet" href="(:/:)/vendor/ie10-viewport-bug-workaround.css">
     <!-- Prism styles -->
     <link rel="stylesheet" href="(:/:)/vendor/prism/prism.css"/>
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="(:/:)/css/styles.css"/>
-
+    
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link rel="stylesheet" href="(:/:)/vendor/ie10-viewport-bug-workaround.css">
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="(:/:)(:/:)/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!--[if lt IE 9]><script src="(:/:)/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="(:/:)/vendor/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
@@ -54,8 +53,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- <script type="text/javascript" src="(:/:)/bower_components/bootstrap/dist/js/bootstrap.js"></script> -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="(:/:)/vendor/prism/prism.js"></script>
     <!-- <script type="text/javascript" src="(:/:)/vendor/docs.min.js"></script> -->
     <script type="text/javascript" src="(:/:)/vendor/ie10-viewport-bug-workaround.js"></script>
-    <script type="text/javascript" src="(:/:)/vendor/prism/prism.js"></script>
   </body>
 </html>
